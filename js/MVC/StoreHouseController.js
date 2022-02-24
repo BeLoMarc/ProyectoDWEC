275fbcfd13add4313d5c70afef487e8bfd9a7fe4
BeLoMarc <blmarcos2k@gmail.com>
2022-02-24 16:57:47 +0100

    nuevo comienzo

--- js/MVC/StoreHouseController.js
+++ js/MVC/StoreHouseController.js
@@ -1,23 +1,15 @@
 'use strict'
  class StoreHouseController {
-    //campos privados
-    #modelStoreHouse;
-    #viewStoreHouse;
-    constructor(modelStoreHouse, viewStoreHouse) {
-        this.#modelStoreHouse =modelStoreHouse;
-        this.#viewStoreHouse =viewStoreHouse;
-        //esto llama al on init de abajo que llama al init de storehouseview
-        // que es el que carga el html
-        this.onInit();
+    
+    constructor() {
+        
     }
     /**Desde el Controlador necesitamos un método que invoque el método que acabamos de crear
  en la Vista. Como nomenclatura para nombrar este tipo de métodos vamos a utilizar la
  preposición on en respuesta a los eventos que nos ocurran en el Controlador. Estos eventos los
  tenemos que invocar en respuesta a un cambio de datos en el Modelo. */
 
-    onInit = () => {
-        this.#viewStoreHouse.init();
-    }
+    
 
 
 }
